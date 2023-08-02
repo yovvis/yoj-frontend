2d92f35d27c1f6b55087c9c2568e285c0c827678
tyangjian <tyangjian@epoint.com.cn>
2023-08-02 19:12:29 +0800

    update openAPI生成前端文件

--- src/store/user.ts
+++ src/store/user.ts
@@ -1,5 +1,6 @@
 import { StoreOptions } from "vuex";
 import AccessEnum from "@/access/accessEnum";
+import { UserControllerService } from "../../generated";
 
 export default {
   namespaced: true,
@@ -7,7 +8,6 @@ export default {
   state: () => ({
     loginUser: {
       userName: "未登录",
-      userRole: AccessEnum.NOT_LOGIN,
     },
   }),
 
@@ -16,9 +16,17 @@ export default {
 
   //action
   actions: {
-    getLoginUser({ commit, state }, payload) {
+    async getLoginUser({ commit, state }, payload) {
       // 改成远程登录
-      commit("updateUser", payload);
+      const resp = await UserControllerService.getLoginUserUsingGet();
+      if (resp.code === 0) {
+        commit("updateUser", resp.data);
+      } else {
+        commit("updateUser", {
+          ...state.loginUser,
+          userRole: AccessEnum.NOT_LOGIN,
+        });
+      }
     },
   },
   // mutations
